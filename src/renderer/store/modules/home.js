791d70f2af80275325de38297c97d9451ef32ff3
microgram <maogang2014@126.com>
2019-11-04 18:09:07 +0800

    添加串口重连功能

--- src/renderer/store/modules/home.js
+++ src/renderer/store/modules/home.js
@@ -1,9 +1,9 @@
-import { EventBus } from '../../lib/event'
-import { parse } from '../../lib/parse'
-const collection = require('lodash/collection')
-const net = require('net-socket')
-const SerialPort = require('serialport')
-const moment = require('moment')
+import { EventBus } from '../../lib/event';
+import { parse } from '../../lib/parse';
+const collection = require('lodash/collection');
+const net = require('net-socket');
+const SerialPort = require('serialport');
+const moment = require('moment');
 const state = {
   windowSize: { height: window.innerHeight, width: window.innerWidth },
   comNumber: 30,
@@ -15,169 +15,188 @@ const state = {
   netIsDisabled: false,
   infos: [],
   displayState: '暂停'
-}
+};
 
-let hex = true
-let ascii = false
-let timestamp = false
-let isDisplay = true
+let hex = true;
+let ascii = false;
+let timestamp = false;
+let isDisplay = true;
 const mutations = {
   WINDOW_SIZE(state, value) {
-    state.windowSize = value
+    state.windowSize = value;
   },
   SERIAL_STATE(state, value) {
-    state.serialState = value
-    state.serialIsDisabled = value !== '未连接'
+    state.serialState = value;
+    state.serialIsDisabled = value !== '未连接';
   },
   PACKAGE_TIME(state, value) {
-    state.packageTime = value
+    state.packageTime = value;
   },
   SERIAL_BAUDRATE(state, value) {
-    state.serialBaudrate = value
+    state.serialBaudrate = value;
   },
   NET_STATE(state, value) {
-    state.netState = value
-    state.netIsDisabled = value !== '未开启'
+    state.netState = value;
+    state.netIsDisabled = value !== '未开启';
   },
   DISPLAY_STATE(state) {
-    isDisplay = !isDisplay
-    state.displayState = isDisplay ? '暂停' : '开启'
+    isDisplay = !isDisplay;
+    state.displayState = isDisplay ? '暂停' : '开启';
   },
   DISPLAY_CLEAR(state) {
-    state.infos = []
+    state.infos = [];
   },
   DISPLAY_CONTENT(state, value) {
-    if (!isDisplay) return
-    if (state.infos.length > 1000) state.infos = []
-    let now = ''
-    if (timestamp) now = moment().format('YYYY-MM-DD HH:mm:ss.SSS')
+    if (!isDisplay) return;
+    if (state.infos.length > 1000) state.infos = [];
+    let now = '';
+    if (timestamp) now = moment().format('YYYY-MM-DD HH:mm:ss.SSS');
     if (hex) {
       state.infos.push(
         `${value.type}-Hex[${now}]:  ${value.data.toString('hex')}`
-      )
+      );
     }
     if (ascii) {
-      state.infos.push(`${value.type}-Ascii[${now}]: ${value.data.toString()}`)
+      state.infos.push(`${value.type}-Ascii[${now}]: ${value.data.toString()}`);
     }
   }
-}
+};
 
-let port
-let socket
-let isConnect
-let sendInterval
+let port;
+let socket;
+let isConnect;
+let sendInterval, reconnectInterval;
 const actions = {
   actionWindowSize({ commit }, value) {
-    commit('WINDOW_SIZE', value)
+    commit('WINDOW_SIZE', value);
   },
   actionCheckedChange({ commit }, value) {
     if (value[0] === 'hex' || value[1] === 'hex' || value[2] === 'hex') {
-      hex = true
-    } else hex = false
+      hex = true;
+    } else hex = false;
     if (value[0] === 'ascii' || value[1] === 'ascii' || value[2] === 'ascii') {
-      ascii = true
-    } else ascii = false
+      ascii = true;
+    } else ascii = false;
     if (
       value[0] === 'timestamp' ||
       value[1] === 'timestamp' ||
       value[2] === 'timestamp'
     ) {
-      timestamp = true
-    } else timestamp = false
+      timestamp = true;
+    } else timestamp = false;
   },
   actionDisplayPause({ commit }) {
-    commit('DISPLAY_STATE')
+    commit('DISPLAY_STATE');
   },
   actionDisplayClear({ commit }) {
-    commit('DISPLAY_CLEAR')
+    commit('DISPLAY_CLEAR');
   },
   actionSerial({ commit, state }, value) {
+    // 清除当前所有状态
+    if (port && port.isOpen) port.close();
+    clearInterval(sendInterval);
+    clearInterval(reconnectInterval);
     if (state.serialState === '已连接') {
-      port.close()
-      port = null
-      clearInterval(sendInterval)
-      commit('SERIAL_STATE', '未连接')
-      return
+      commit('SERIAL_STATE', '未连接');
+      return;
     }
-    commit('SERIAL_BAUDRATE', value.baudRate)
+    commit('SERIAL_BAUDRATE', value.baudRate);
     port = new SerialPort(value.port, {
       baudRate: value.baudRate,
       autoOpen: false
-    })
+    });
     port.on('error', err => {
-      EventBus.$emit('message-box', '串口错误：' + err)
-      port.close()
-      commit('SERIAL_STATE', '未连接')
-    })
+      EventBus.$emit('message-box', '串口错误：' + err);
+      port.close();
+      commit('SERIAL_STATE', '未连接');
+    });
     port.open(err => {
-      if (err) EventBus.$emit('message-box', '打开串口失败：' + err)
-      else commit('SERIAL_STATE', '已连接')
-    })
-    let data
+      if (err) EventBus.$emit('message-box', '打开串口失败：' + err);
+      else commit('SERIAL_STATE', '已连接');
+    });
+    // FIXME 10秒检查一次串口，防止掉线
+    reconnectInterval = setInterval(() => {
+      if (state.serialState !== '已连接') {
+        clearInterval(reconnectInterval);
+        return;
+      }
+      if (!port || !port.isOpen) {
+        port = new SerialPort(
+          value.port,
+          {
+            baudRate: value.baudRate
+          },
+          err => {
+            if (err) EventBus.$emit('message-box', '串口重连错误：' + err);
+          }
+        );
+      }
+    }, 30 * 1000);
+    let data;
     sendInterval = setInterval(() => {
-      if (!port) return
-      let msg = port.read()
+      if (!port || !port.isOpen) return;
+      let msg = port.read();
       if (msg) {
-        if (data) data = Buffer.concat([data, msg])
-        else data = msg
+        if (data) data = Buffer.concat([data, msg]);
+        else data = msg;
       }
       if (isConnect && data && (!msg || data.length > 1024)) {
-        let ret = parse(value.dataType, data) // FIXME 循环解析方法
+        let ret = parse(value.dataType, data); // FIXME 循环解析方法
         // 剩余长度部分,超时剩余部分清空
-        if (msg) data = ret.residual
-        else data = null
+        if (msg) data = ret.residual;
+        else data = null;
         collection.forEach(ret.data, (value, key) => {
           if (value) {
             setTimeout(() => {
               socket.write(value, err => {
-                if (err) console.error(`socket write:${err}`)
-                else commit('DISPLAY_CONTENT', { type: 'Send', data: value })
-              })
-            }, key * 20) // 延时，保证每包数据独立发送
+                if (err) console.error(`socket write:${err}`);
+                else commit('DISPLAY_CONTENT', { type: 'Send', data: value });
+              });
+            }, key * 20); // 延时，保证每包数据独立发送
           }
-        })
-      } else if (data && data.length > 2048) data = null
-    }, value.packageTime)
+        });
+      } else if (data && data.length > 2048) data = null;
+    }, value.packageTime);
   },
   actionNet({ commit, state }, value) {
     if (state.netState === '已开启') {
-      isConnect = false
-      socket.destroy()
-      commit('NET_STATE', '未开启')
-      return
+      isConnect = false;
+      socket.destroy();
+      commit('NET_STATE', '未开启');
+      return;
     }
-    commit('NET_STATE', '已开启')
-    socket = net.connect(Number(value.casterPort), value.casterIp) // reconnect
+    commit('NET_STATE', '已开启');
+    socket = net.connect(Number(value.casterPort), value.casterIp); // reconnect
     socket.on('data', data => {
       if (isConnect) {
         if (port && port.isOpen) {
           port.write(data, err => {
-            if (err) console.error(`port write:${err}`)
-            else commit('DISPLAY_CONTENT', { type: 'Receive', data: data })
-          })
+            if (err) console.error(`port write:${err}`);
+            else commit('DISPLAY_CONTENT', { type: 'Receive', data: data });
+          });
         }
       } else {
         if (data.toString().includes('ICY 200 OK')) {
-          isConnect = true
-          EventBus.$emit('message-box', '连接服务器成功')
+          isConnect = true;
+          EventBus.$emit('message-box', '连接服务器成功');
         } else {
-          isConnect = false
-          EventBus.$emit('message-box', `连接服务器失败:${data.toString()}`)
+          isConnect = false;
+          EventBus.$emit('message-box', `连接服务器失败:${data.toString()}`);
         }
       }
-    })
+    });
     socket.on('close', () => {
-      isConnect = false
-    })
+      isConnect = false;
+    });
     socket.on('connect', () => {
-      socket.write(`SOURCE ${value.password} ${value.mountpoint}\r\n`)
-    })
+      socket.write(`SOURCE ${value.password} ${value.mountpoint}\r\n`);
+    });
   }
-}
+};
 
 export default {
   namespaced: true,
   state,
   mutations,
   actions
-}
+};
--- src/renderer/view/home/home.js
+++ src/renderer/view/home/home.js
@@ -1,8 +1,8 @@
-import { mapState, mapActions } from 'vuex'
-import { EventBus } from '@/lib/event'
-const collection = require('lodash/collection')
-const SerialPort = require('serialport')
-const os = require('os')
+import { mapState, mapActions } from 'vuex';
+import { EventBus } from '@/lib/event';
+const collection = require('lodash/collection');
+const SerialPort = require('serialport');
+const os = require('os');
 export default {
   name: 'home',
   data() {
@@ -11,7 +11,7 @@ export default {
       coms: [],
       baudRate: 9600,
       baudRates: [4800, 9600, 115200],
-      packageTime: 50,
+      packageTime: 100,
       packageTimes: [50, 100, 200, 500, 1000],
       dataTypes: ['ublox'],
       dataType: 'ublox',
@@ -23,53 +23,53 @@ export default {
       userId: '',
       password: '12345678',
       checkedDisplay: ['hex']
-    }
+    };
   },
   watch: {
     // 使用箭头函数时this无法识别
     infosChange: function() {
       this.$nextTick(() => {
-        let infosArea = document.getElementById('infos-display')
-        infosArea.scrollTop = infosArea.scrollHeight
-      })
+        let infosArea = document.getElementById('infos-display');
+        infosArea.scrollTop = infosArea.scrollHeight;
+      });
     }
   },
   created() {
     // 获取COM
     setInterval(() => {
       SerialPort.list().then(res => {
-        this.coms = []
+        this.coms = [];
         collection.forEach(res, value => {
-          this.coms.push(value.comName)
-        })
-      })
-    }, 3000)
+          this.coms.push(value.comName);
+        });
+      });
+    }, 3000);
     // 读取ip
-    let netParams = os.networkInterfaces()
+    let netParams = os.networkInterfaces();
     collection.forEach(netParams, value => {
-      let temp = collection.filter(value, { family: 'IPv4' })
-      if (temp[0].address) this.casterIps.push(temp[0].address)
-    })
+      let temp = collection.filter(value, { family: 'IPv4' });
+      if (temp[0].address) this.casterIps.push(temp[0].address);
+    });
     // 监听错误事件
     EventBus.$on('message-box', value => {
       this.$Message.warning({
         content: value,
         duration: 5
-      })
-    })
+      });
+    });
     // 监听窗口尺寸变化
     window.onresize = () => {
       this.actionWindowSize({
         height: window.innerHeight,
         width: window.innerWidth
-      })
-    }
+      });
+    };
     window.onload = () => {
       this.actionWindowSize({
         height: window.innerHeight,
         width: window.innerWidth
-      })
-    }
+      });
+    };
   },
   computed: {
     ...mapState('home', [
@@ -83,7 +83,7 @@ export default {
       'infos'
     ]),
     infosChange() {
-      return this.infos
+      return this.infos;
     }
   },
   methods: {
@@ -97,20 +97,20 @@ export default {
     ]),
     handleSerial() {
       if (!this.comSelected || !this.baudRate || !this.packageTime) {
-        this.$Message.warning('请填写串口参数')
-        return
+        this.$Message.warning('请填写串口参数');
+        return;
       }
       this.actionSerial({
         port: this.comSelected,
         baudRate: Number(this.baudRate),
         packageTime: Number(this.packageTime),
         dataType: this.dataType
-      })
+      });
     },
     handleNet() {
       if (!this.casterIp || !this.casterPort || !this.mountpoint) {
-        this.$Message.warning('请填写服务器参数')
-        return
+        this.$Message.warning('请填写服务器参数');
+        return;
       }
       this.actionNet({
         casterIp: this.casterIp,
@@ -118,16 +118,16 @@ export default {
         mountpoint: this.mountpoint,
         userId: this.userId,
         password: this.password
-      })
+      });
     },
     checkedChange(value) {
-      this.actionCheckedChange(value)
+      this.actionCheckedChange(value);
     },
     displayPause() {
-      this.actionDisplayPause()
+      this.actionDisplayPause();
     },
     displayClear() {
-      this.actionDisplayClear()
+      this.actionDisplayClear();
     }
   }
-}
+};
